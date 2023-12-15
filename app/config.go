0071744fbf36e0a0835d788d1fa77a56a65243b0
Yadija <ayuliadi01@gmail.com>
2023-12-15 10:54:10 +0800

    ⚒️build: setup file config

---
+++ app/config.go
@@ -0,0 +1,19 @@
+package app
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func Config(key string) string {
+	// load .env file
+	config := viper.New()
+	config.SetConfigFile(".env")
+	err := config.ReadInConfig()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	return config.GetString(key)
+}
