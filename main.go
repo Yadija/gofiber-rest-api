bf143956a2ed8f4c8a9fe53834bcdc62cc9c9f66
Yadija <ayuliadi01@gmail.com>
2023-12-16 09:02:36 +0800

    ⚒️build: make error handling

--- main.go
+++ main.go
@@ -8,7 +8,21 @@ import (
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			code := fiber.StatusInternalServerError // status code defaults to 500
+			// retrieve the status code if it's an *fiber.Error
+			if exception, ok := err.(*fiber.Error); ok {
+				code = exception.Code
+			}
+
+			// send custom errors as JSON
+			return ctx.Status(code).JSON(fiber.Map{
+				"status":  code,
+				"message": err.Error(),
+			})
+		},
+	})
 
 	// connection to database
 	database.OpenConnection()
