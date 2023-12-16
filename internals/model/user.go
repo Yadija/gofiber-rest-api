f0e78f9ad133dc3c5669bf59dd6e71b0c12fadf1
Yadija <ayuliadi01@gmail.com>
2023-12-16 19:16:39 +0800

    📦refactor: add default for token in user model

--- internals/model/user.go
+++ internals/model/user.go
@@ -4,7 +4,7 @@ type User struct {
 	Username string   `gorm:"column:username;primary_key;type:varchar(100);not null" json:"username" validate:"required,min=5,max=100,alphanum"`
 	Password string   `gorm:"column:password;type:text;not null" json:"password" validate:"required,min=6,max=100"`
 	Fullname string   `gorm:"column:fullname;type:varchar(100);not null" json:"fullname" validate:"required,min=5,max=100"`
-	Token    string   `gorm:"column:token;type:text" json:"token"`
+	Token    string   `gorm:"column:token;type:text;default:null" json:"token"`
 	Threads  []Thread `gorm:"foreignKey:owner;references:username"`
 }
 
