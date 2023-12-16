2f63e68beedff6d8b785eea027f83f05eb1b8f92
Yadija <ayuliadi01@gmail.com>
2023-12-16 22:27:11 +0800

    ⚒️build: make put auth

--- internals/handler/auth/auth.go
+++ internals/handler/auth/auth.go
@@ -4,6 +4,9 @@ import (
 	"gofiber-restapi/database"
 	"gofiber-restapi/helper"
 	"gofiber-restapi/internals/model"
+	"strings"
+
+	jtoken "github.com/golang-jwt/jwt/v4"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
@@ -51,6 +54,48 @@ func Login(ctx *fiber.Ctx) error {
 		"status":  "success",
 		"message": "Login success",
 		"data": map[string]interface{}{
+			"accessToken":  accessToken,
+			"refreshToken": refreshToken,
+		},
+	})
+}
+
+func RefreshToken(ctx *fiber.Ctx) error {
+	db := database.DB.Db
+	user := new(model.User)
+
+	// get refresh token
+	getToken := ctx.Request().Header.Peek("Authorization")
+	refreshToken := strings.TrimPrefix(string(getToken), "Bearer ")
+
+	// get data from token
+	claims := ctx.Locals("user").(*jtoken.Token).Claims.(jtoken.MapClaims)
+
+	// get user
+	if err := db.Where("username = ?", claims["username"]).First(&user).Error; err != nil {
+		return fiber.NewError(fiber.StatusUnauthorized, "Access denied")
+	}
+
+	// verify refresh token
+	if user.Token != refreshToken {
+		return fiber.NewError(fiber.StatusUnauthorized, "Access denied")
+	}
+
+	// generate tokens
+	accessToken, refreshToken, err := helper.GenerateTokens(user.Username)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	// update refresh token to database
+	if err := db.Model(&user).Update("token", refreshToken).Error; err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Failed to update refresh token")
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  "success",
+		"message": "Refresh token success",
+		"data": map[string]string{
 			"accessToken":  accessToken,
 			"refreshToken": refreshToken,
 		},
--- internals/routes/auth/auth.go
+++ internals/routes/auth/auth.go
@@ -2,11 +2,15 @@ package authroutes
 
 import (
 	authhandler "gofiber-restapi/internals/handler/auth"
+	"gofiber-restapi/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupRoutes(router fiber.Router) {
+	refreshJWT := middleware.NewAuthMiddleware("REFRESH_TOKEN_KEY")
+
 	auth := router.Group("/auth")
 	auth.Post("/", authhandler.Login)
+	auth.Put("/", refreshJWT, authhandler.RefreshToken)
 }
